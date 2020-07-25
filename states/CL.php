fb878e4528e6d404fbca95f7f5394410649f5e8b
LtM0 <luukgremmen@me.com>
2020-07-25 10:10:09 -0400

    Update and rename CO.php to CL.php

---
+++ states/CL.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Chile states
+ *
+ * @author   Luuk Gremmen <luukgremmen@gmail.com>
+ * @version  0.0.1
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+
+global $states;
+
+$states ['CL' ] = array (
+	'RM' => 'Metropolitana de Santiago'
+	);
