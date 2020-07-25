f9c49c9933242c97c91c9217d555233f040a2080
LtM0 <luukgremmen@me.com>
2020-07-25 09:26:29 -0400

    Update and rename CO.php to CL.php

---
+++ places/CL.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Chile places
+ *
+ * @author   Luuk Gremmen <luukgremmen@gmail.com>
+ * @version  0.0.1
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+global $places;
+
+$places['CL'] = array(
+	'RM' => array(
+	"Chicureo, Colina",
+        "La Reina",
+        "Las Condes",
+        "Lo Barnechea",
+        "Ñuñoa",
+        "Piedra Roja, Colina",
+        "Providencia",
+		),
+	);
